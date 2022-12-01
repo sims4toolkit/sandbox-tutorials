6356ffcd8043da1f3ce307f56c598ee12cf5737e
Frank Kulak <fs.kulak@gmail.com>
2022-12-01 14:16:54 -0500

    Update sandbox page 3.

--- src/get-to-know-sandbox/2.cjs
+++ src/get-to-know-sandbox/2.cjs
@@ -1,16 +1,15 @@
-const { Package } = require("@s4tk/models");
-const { TuningResourceType } = require("@s4tk/models/enums");
-const { fnv64 } = require("@s4tk/hashing");
+// you can import S4TK modules with `require()`
+const { StringTableResource } = require("@s4tk/models");
+const { formatStringKey } = require("@s4tk/hashing/formatting");
 
-const pkg = new Package();
+// import the STBL data, as you saw on the last page
+const buffer = await Sandbox.import("Sample.stbl");
 
-Buffer.from("This is going to be a file.");
+// now that you've imported StringTableResource, you can parse the buffer
+const stbl = StringTableResource.from(buffer);
 
-pkg.add(
-  {
-    type: TuningResourceType.Tuning,
-    group: 0,
-    instance: fnv64("something"),
-  }
-  // TODO:
-);
+// this line gets the first entry in the string table
+const entry = stbl.entries[0];
+
+// see what the key/value of the entry are!
+Sandbox.output(`Key: ${formatStringKey(entry.key)}`, `Value: ${entry.value}`);
