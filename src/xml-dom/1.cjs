3dfc42eb537b82907b8ba04350173dcce150d521
Frank Kulak <fs.kulak@gmail.com>
2022-12-03 15:51:37 -0500

    Update second page of XML DOM tut.

--- src/xml-dom/1.cjs
+++ src/xml-dom/1.cjs
@@ -1,1 +1,22 @@
-// TODO
+const { XmlDocumentNode, XmlElementNode } = require("@s4tk/xml-dom");
+
+// This is the same doc loaded on the last page, but we're using the `child`
+// property to get the root immediately -- remember, `root` is the <I> node,
+// which is an instance of the XmlElementNode class
+const root = XmlDocumentNode.from(`
+<I c="Class" i="type" m="path" n="some_tuning" s="12345">
+  <L n="list_of_tuples">
+    <U>
+      <T n="tunable">67890<!--something--></T>
+      <E n="enum_value">VALUE_1</E>
+    </U>
+    <U>
+      <T n="tunable">24680<!--something_else--></T>
+      <E n="enum_value">VALUE_2</E>
+    </U>
+  </L>
+</I>
+`).child;
+
+// Just for posterity, let's check the tag value again, make sure it's "I"
+Sandbox.test('Root tag should be "I"', root.tag === "I");
