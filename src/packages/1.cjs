90a256f3a19fb811b68b19ed7e0f4eab447a7202
Frank Kulak <fs.kulak@gmail.com>
2022-12-08 22:25:38 -0500

    Update packages tutorial.

--- src/packages/1.cjs
+++ src/packages/1.cjs
@@ -1,25 +1,26 @@
 const { Package } = require("@s4tk/models");
 
 // ==================================================
-// Reading a Package Binary
+// Reading a Package
 
-// first, you need to get a buffer containing DBPF data in the sandbox, use
-// `Sandbox.import(filename)`
+// First, you need to get a buffer containing DBPF data
+// This tutorial provides a package called "Sample.package"
 const buffer = await Sandbox.import("Sample.package");
 
-// then, you can use the static `Package.from()` method to create a Package
+// Then, you can use the static `Package.from()` method to create a Package
 const pkg = Package.from(buffer);
 
-// just to confirm that the pkg is loaded, check its size
+// Just to confirm that the pkg is loaded, check its size
 Sandbox.output(`Loaded size: ${pkg.size}`);
 
 // ==================================================
-// Writing a Package Binary
+// Writing a Package
 
-// to get the buffer for a package, use `getBuffer()`
+// To get the buffer for a package, use `getBuffer()`
 const outputBuffer = pkg.getBuffer();
 
-// once you have the buffer, you can use `Sandbox.download()`
+// Once you have the buffer, you can use `Sandbox.download()`
 Sandbox.download("Sample.package", outputBuffer);
 
-// remember, in Node, you must use the `fs` module to read and write files
+// Remember: In Node, you must use the `fs` module to read and write files,
+// since `import()` and `download()` are specific to the sandbox.
